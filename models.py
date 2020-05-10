63e8054e9e6c056f88aab8a0d0bdc040d0c389ab
Aleksandr Datsko <alexandr.datsko@gmail.com>
2020-05-10 15:39:05 +0300

    add models and migrate tables, .gitignore and .env.sample

---
+++ models.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    login = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+
+class Book(db.Model):
+    __tablename__ = "books"
+    id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
