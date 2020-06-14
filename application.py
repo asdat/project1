181aed29ea7bacdaf8d776b415ee2572cb83c86c
Aleksandr Datsko <alexandr.datsko@gmail.com>
2020-06-14 11:57:48 +0300

    move to raw SQL queries, minor code improvements

--- application.py
+++ application.py
@@ -1,12 +1,11 @@
-import os, hashlib, requests
+import os, hashlib, requests, datetime
 from math import ceil
 
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask import Flask, flash, render_template, request, redirect, url_for, session
 from flask_session import Session
-from sqlalchemy import create_engine, or_, asc, func
-from sqlalchemy.orm import sessionmaker
-
-from models import *
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 app = Flask(__name__)
 
@@ -19,12 +18,11 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
 Session(app)
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"), echo=False)
-db_session = sessionmaker(bind=engine)()
+db = scoped_session(sessionmaker(engine))
 
 
 @app.route("/", methods=["GET"])
@@ -34,7 +32,8 @@ def home():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if session['user']:
+    if session.get('user_id'):
+        flash("You already logged in")
         return redirect(url_for('home'))
 
     if request.method == 'POST':
@@ -42,7 +41,7 @@ def register():
         login = request.form.get("login")
         password = request.form.get("password")
 
-        user = db_session.query(User).filter_by(login=login).first()
+        user = db.execute("SELECT id FROM users WHERE login = :login", {"login": login}).fetchone()
         if user:
             return render_template("register.html", message=f"User with login '{login}' already exists.", name=name,
                                    login=login, password=password)
@@ -52,9 +51,11 @@ def register():
                                        login=login, password=password)
 
             try:
-                db.session.add(User(name=name, login=login, password=hashlib.sha1(password.encode()).hexdigest()))
-                print(f"Added user {name} ({login}) by {password}.")
-                db.session.commit()
+                db.execute("INSERT INTO users (name, login, password) VALUES (:name, :login, :password)",
+                           {"name": name, "login": login,
+                            "password": hashlib.sha1(password.encode()).hexdigest()})
+                db.commit()
+                flash("Registered successful")
             except Exception:
                 return render_template("register.html", message="Error creating user, please try again later.",
                                        name=name, login=login, password=password)
@@ -66,7 +67,8 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if session['user']:
+    if session.get('user_id'):
+        flash("You already logged in")
         return redirect(url_for('home'))
 
     if request.method == 'POST':
@@ -74,43 +76,59 @@ def login():
         password = request.form.get("password")
 
         if not login:
-            return render_template("login.html", message="Login is missing.")
+            return render_template("login.html", message="Login is missing.", login=login, password=password)
 
         if not password:
-            return render_template("login.html", message="Password is missing.")
+            return render_template("login.html", message="Password is missing.", login=login, password=password)
 
-        user = db_session.query(User).filter_by(login=login).first()
+        user = db.execute("SELECT id, name, login, password FROM users WHERE login = :login",
+                          {"login": login}).fetchone()
         if user.password == hashlib.sha1(password.encode()).hexdigest():
-            session['user'] = user
+            session['user_id'] = user.id
+            session['user_name'] = f"{user.name} ({user.login})"
             return redirect(url_for('home'))
         else:
-            return render_template("login.html", message="Password incorrect.")
+            return render_template("login.html", message="Password incorrect.", login=login, password=password)
 
     return render_template("login.html")
 
 
 @app.route("/logout", methods=["GET"])
 def logout():
-    session['user'] = None
+    session['user_id'] = None
+    session['user_name'] = ''
     return redirect(url_for('home'))
 
 
 @app.route("/book/<isbn>", methods=["GET"])
 def book(isbn):
-    if not session['user']:
+    if not session.get('user_id'):
+        flash("Please login first")
         return redirect(url_for('home'))
 
     if not isbn:
         return render_template("book.html", message="No ISBN was provided")
 
-    book_item = db_session.query(Book).filter_by(isbn=isbn).first()
+    book_item = db.execute("SELECT id, title, isbn, author, year FROM books WHERE isbn = :isbn",
+                           {"isbn": isbn}).fetchone()
 
     if not book_item:
-        return render_template("404.html")
+        return render_template("404.html"), 404
 
-    has_review = db_session.query(Review).filter_by(book_id=book_item.id).filter_by(user_id=session['user'].id).first()
-    rating = db_session.query(func.avg(Review.rating)).filter_by(book_id=book_item.id).scalar()
-    count = db_session.query(func.count(Review.rating)).filter_by(book_id=book_item.id).scalar()
+    reviews = db.execute(
+        "SELECT r.rating, r.comment, u.name, u.login "
+        "FROM reviews AS r "
+        "INNER JOIN users AS u ON u.id = r.user_id "
+        "WHERE r.book_id = :book_id",
+        {"book_id": book_item.id}).fetchall()
+
+    has_review = db.execute(
+        "SELECT COUNT(*) as cnt FROM reviews WHERE book_id = :book_id AND user_id = :user_id",
+        {"user_id": session['user_id'], "book_id": book_item.id}).fetchone().cnt > 0
+
+    rating = db.execute(
+        "SELECT AVG(rating) as average, COUNT(*) as count FROM reviews WHERE book_id = :book_id",
+        {"book_id": book_item.id}).fetchone()
 
     res = requests.get("https://www.goodreads.com/book/review_counts.json",
                        params={"key": os.getenv("GOODREADS_KEY"), "isbns": isbn})
@@ -124,48 +142,51 @@ def book(isbn):
         goodreads['count'] = data.get('work_ratings_count')
         goodreads['rating'] = data.get('average_rating')
 
-    return render_template("book.html", book=book_item, has_review=has_review, rating=rating, count=count,
+    return render_template("book.html", book=book_item, has_review=has_review, reviews=reviews, rating=rating,
                            goodreads=goodreads)
 
 
 @app.route("/api/<isbn>", methods=["GET"])
 def api(isbn):
-    if not isbn:
-        return jsonify({'error': 'no isbn provided'})
-
-    book_item = db_session.query(Book).filter_by(isbn=isbn).first()
+    book_item = db.execute("SELECT id, title, isbn, author, year FROM books WHERE isbn = :isbn",
+                           {"isbn": isbn}).fetchone()
 
     if not book_item:
-        return jsonify({'error': f"no book for ISBN {isbn} was found"})
+        return {'error': f"no book for ISBN {isbn} was found"}, 404
 
-    rating = db_session.query(func.avg(Review.rating)).filter_by(book_id=book_item.id).scalar()
-    count = db_session.query(func.count(Review.rating)).filter_by(book_id=book_item.id).scalar()
+    rating = db.execute(
+        "SELECT AVG(rating) as average, COUNT(*) as count FROM reviews WHERE book_id = :book_id",
+        {"book_id": book_item.id}).fetchone()
 
-    return jsonify({
+    return {
         'title': book_item.title,
         'author': book_item.author,
         'year': book_item.year,
         'isbn': book_item.isbn,
-        'review_count': count,
-        'average_score': float(round(rating, 2))
-    })
+        'review_count': rating.count,
+        'average_score': float(round(rating.average, 2))
+    }
 
 
 @app.route("/review", methods=["POST"])
 def review():
-    if not session['user']:
+    if not session.get('user_id'):
+        flash("Please login first")
         return redirect(url_for('home'))
 
     rating = request.form.get("rating")
     comment = request.form.get("comment")
     isbn = request.form.get("isbn")
 
-    book_item = db_session.query(Book).filter_by(isbn=isbn).first()
+    book_item = db.execute("SELECT id, title, isbn, author, year FROM books WHERE isbn = :isbn",
+                           {"isbn": isbn}).fetchone()
     if not book_item:
         return render_template("book.html", message=f"No book fo ISBN {isbn} was found")
 
-    review_item = db_session.query(Review).filter_by(book_id=book_item.id).filter_by(user_id=session['user'].id).first()
-    if review_item:
+    has_review = db.execute(
+        "SELECT COUNT(*) as cnt FROM reviews WHERE book_id = :book_id AND user_id = :user_id",
+        {"user_id": session['user_id'], "book_id": book_item.id}).fetchone().cnt > 0
+    if has_review:
         return render_template("book.html", message="You already left review for this book")
 
     if not rating:
@@ -173,10 +194,14 @@ def review():
                                comment=comment)
 
     try:
-        db.session.add(Review(rating=rating, comment=comment, book_id=book_item.id, user_id=session['user'].id))
-        print(
-            f"Added review {rating} with comment {comment} by {session['user'].name} ({session['user'].login}) for book {book_item.title} ({isbn}).")
-        db.session.commit()
+        db.execute("INSERT INTO reviews (rating, comment, book_id, user_id, created_at) "
+                   "VALUES (:rating, :comment, :book_id, :user_id, :created_ad)",
+                   {"rating": rating, "comment": comment, "book_id": book_item.id,
+                    "user_id": session['user_id'], "created_ad": datetime.datetime.utcnow()})
+
+        flash(
+            f"Added review {rating} with comment {comment} by {session['user_name']} for book {book_item.title} ({isbn}).")
+        db.commit()
     except Exception:
         return render_template("book.html", message="Error creating review, please try again later.", book=book_item,
                                rating=rating, comment=comment)
@@ -186,7 +211,8 @@ def review():
 
 @app.route("/search", methods=["GET"])
 def search():
-    if not session['user']:
+    if not session.get('user_id'):
+        flash("Please login first")
         return redirect(url_for('home'))
 
     per_page = 10
@@ -196,17 +222,22 @@ def search():
     if not search:
         return render_template("search.html")
 
-    query = db_session.query(Book).filter(or_(
-        Book.isbn.like(f'%{search}%'),
-        Book.title.like(f'%{search}%'),
-        Book.author.like(f'%{search}%'))
-    )
-    count = query.count()
-    books_list = query.order_by(asc(Book.isbn)).limit(per_page).offset(per_page * (page - 1))
+    results_count = db.execute("SELECT COUNT(id) as cnt "
+                               "FROM books "
+                               "WHERE isbn LIKE :search OR title LIKE :search OR author LIKE :search",
+                               {"search": f"%{search}%"}).fetchone().cnt
 
-    if books_list.count() > 0:
-        return render_template("search.html", search=search, books=books_list, pages=ceil(count / per_page),
-                               count=count, page=page, per_page=per_page)
+    books_list = db.execute("SELECT title, isbn, author, year "
+                            "FROM books "
+                            "WHERE isbn LIKE :search OR title LIKE :search OR author LIKE :search "
+                            "ORDER BY year ASC, isbn ASC "
+                            "LIMIT :limit OFFSET :offset",
+                            {"search": f"%{search}%", "offset": per_page * (page - 1),
+                             "limit": per_page}).fetchall()
+
+    if results_count > 0:
+        return render_template("search.html", search=search, books=books_list, pages=ceil(results_count / per_page),
+                               count=results_count, page=page, per_page=per_page)
 
     else:
         return render_template("search.html", search=search, message=f"No book for search request '{search}' was found")
